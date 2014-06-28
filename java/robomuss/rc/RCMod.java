46d06953d47107bd40dfcb383843d0e4a3edd400
modmuss50 <m123world@gmail.com>
2014-06-28 09:00:24 +0100

    Revert "Added the packet handler from network"
    This reverts commit 979646840d9bb0b8d05363bbbe47ad02d4e4954d.

--- java/robomuss/rc/RCMod.java
+++ java/robomuss/rc/RCMod.java
@@ -6,7 +6,6 @@ import net.minecraft.item.ItemStack;
 import robomuss.rc.block.RCBlocks;
 import robomuss.rc.gui.GuiHandler;
 import robomuss.rc.item.RCItems;
-import robomuss.rc.netty.PacketPipeline;
 import robomuss.rc.proxy.CommonProxy;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -24,9 +23,8 @@ public class RCMod {
 	public static final String MODID = "rc";
 	public static final String NAME = "Rollercoaster Mod";
 	public static final String VERSION = "1.0";
-    public static final PacketPipeline packetPipeline	= new PacketPipeline();
-    
-	@Instance(RCMod.MODID)
+
+	@Instance
 	public static RCMod instance;
 	
 	@SidedProxy(clientSide="robomuss.rc.proxy.ClientProxy", serverSide="robomuss.rc.proxy.CommonProxy")
@@ -71,11 +69,11 @@ public class RCMod {
 	
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
-		packetPipeline.initalise();
+		
 	}
 	
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event) {
-		packetPipeline.postInitialise();
+		
 	}
 }
--- java/robomuss/rc/gui/GuiTrackBuilder.java
+++ java/robomuss/rc/gui/GuiTrackBuilder.java
@@ -1,14 +1,17 @@
 package robomuss.rc.gui;
 
+import java.io.IOException;
+
+import robomuss.rc.block.RCBlocks;
+import robomuss.rc.block.te.TileEntityTrack;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.network.INetHandler;
+import net.minecraft.network.Packet;
+import net.minecraft.network.PacketBuffer;
 import net.minecraft.world.World;
-import robomuss.rc.RCMod;
-import robomuss.rc.block.RCBlocks;
-import robomuss.rc.block.te.TileEntityTrack;
-import robomuss.rc.netty.packets.PacketPlaceBlocks;
 
 public class GuiTrackBuilder extends GuiScreen {
 
@@ -26,10 +29,7 @@ public class GuiTrackBuilder extends GuiScreen {
 		this.y = y;
 		this.z = z;
 		//TileEntityTrackBuilder trackbuilder = (TileEntityTrack) world.getTileEntity(x, y, z);
-
-
-
-
+		
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -44,7 +44,7 @@ public class GuiTrackBuilder extends GuiScreen {
 		buttonList.add(new GuiButton(4, 130, 10, 20, 20, new String("\\").substring(0, 1) + "_"));
 		buttonList.add(new GuiButton(5, 160, 10, 20, 20, new String("\\").substring(0, 1)));
 		buttonList.add(new GuiButton(6, 190, 10, 20, 20, new String("-\\").substring(0, 2)));
-		buttonList.add(new GuiButton(7, 220, 10, 20, 20, "ï¿½"));
+		buttonList.add(new GuiButton(7, 220, 10, 20, 20, "¬"));
 		buttonList.add(new GuiButton(8, 250, 10, 20, 20, "o"));
 		
 		buttonList.add(new GuiButton(9, 10, 40, 20, 20, "< >"));
@@ -62,142 +62,136 @@ public class GuiTrackBuilder extends GuiScreen {
 	protected void actionPerformed(GuiButton button) {
 		int id = button.id;
 		
-
-
-        //TODO call packet with id
-
-
-
-
-
-        for (int j = 0; j < MinecraftServer.getServer().worldServers.length; j++)
-        {
-            System.out.println(MinecraftServer.getServer().worldServers[0].getProviderName());
-        }
-
-
-        System.out.println("hi");
-
-
-        if(world == null)
-            return;
-
-        if(id == 0) {
-            world.setBlock(x, y, z, RCBlocks.flat_track);
-            TileEntityTrack track = (TileEntityTrack) world.getTileEntity(x, y, z);
-            track.direction = direction;
-            world.markBlockForUpdate(x, y, z);
-            if(direction == 0) {
-                z += 1;
-            }
-            else if(direction == 1) {
-                x += 1;
-            }
-            else if(direction == 2) {
-                z -= 1;
-            }
-            else if(direction == 3) {
-                x -= 1;
-            }
-        }
-
-        if(id == 1 || id == 2 || id == 3) {
-            world.setBlock(x, y, z, (id == 1 ? RCBlocks.transition_track : id == 2 ? RCBlocks.sloped_track : RCBlocks.transition_track_2));
-            TileEntityTrack track = (TileEntityTrack) world.getTileEntity(x, y, z);
-            track.direction = direction;
-            world.markBlockForUpdate(x, y, z);
-            if(direction == 0) {
-                z += 1;
-                y += 1;
-            }
-            else if(direction == 1) {
-                x -= 1;
-                y += id > 3 ? -1 : 1;
-            }
-            else if(direction == 2) {
-                z -= 1;
-                y += id > 3 ? -1 : 1;
-            }
-            else if(direction == 3) {
-                x -= 1;
-                y += id > 3 ? -1 : 1;
-            }
-        }
-
-        if(id == 4 || id == 5 || id == 6) {
-            if(id == 6) {
-                y -= 1;
-            }
-            world.setBlock(x, y, z, (id == 4 ? RCBlocks.transition_track : id == 5 ? RCBlocks.sloped_track : RCBlocks.transition_track_2));
-            TileEntityTrack track = (TileEntityTrack) world.getTileEntity(x, y, z);
-            track.direction = direction;
-            world.markBlockForUpdate(x, y, z);
-            if(direction == 0) {
-                z += 1;
-                y += 1;
-            }
-            else if(direction == 1) {
-                x -= 1;
-                y += id > 3 ? -1 : 1;
-            }
-            else if(direction == 2) {
-                z += 1;
-                y += id > 3 ? -1 : 1;
-            }
-            else if(direction == 3) {
-                x -= 1;
-                y += id > 3 ? -1 : 1;
-            }
-            if(id == 4) {
-                y += 1;
-            }
-        }
-
-        if(id == 7) {
-            world.setBlock(x, y, z, RCBlocks.curved_track);
-            TileEntityTrack track = (TileEntityTrack) world.getTileEntity(x, y, z);
-            track.direction = direction;
-            world.markBlockForUpdate(x, y, z);
-            if(direction == 0) {
-                x -= 1;
-                direction = 3;
-            }
-            else if(direction == 1) {
-                direction = 2;
-                z -= 1;
-            }
-            else if(direction == 2) {
-                x += 1;
-                direction = 1;
-            }
-            else if(direction == 3) {
-                z += 1;
-                direction = 0;
-            }
-        }
-
-        if(id == 8) {
-            world.setBlock(x, y, z, RCBlocks.loop);
-            TileEntityTrack track = (TileEntityTrack) world.getTileEntity(x, y, z);
-            track.direction = direction;
-            world.markBlockForUpdate(x, y, z);
-
-            x -= 1;
-            z += 1;
-        }
-
-        if(id == 9) {
-            if(direction == 3) {
-                direction = 0;
-            }
-            else {
-                direction += 1;
-            }
-        }
-
-
-        RCMod.packetPipeline.sendToServer(new PacketPlaceBlocks(this.x, this.y, this.z, id, direction, player));
-
+		Packet packet = new Packet() {
+			
+			@Override
+			public void writePacketData(PacketBuffer pb) throws IOException {
+				
+			}
+			
+			@Override
+			public void readPacketData(PacketBuffer pb) throws IOException {
+				
+			}
+			
+			@Override
+			public void processPacket(INetHandler handler) {
+				
+			}
+		};
+		
+		if(id == 0) {
+			world.setBlock(x, y, z, RCBlocks.flat_track);
+			TileEntityTrack track = (TileEntityTrack) world.getTileEntity(x, y, z);
+			track.direction = direction;
+			world.markBlockForUpdate(x, y, z);
+			if(direction == 0) {
+				z += 1;
+			}
+			else if(direction == 1) {
+				x += 1;
+			}
+			else if(direction == 2) {
+				z -= 1;
+			}
+			else if(direction == 3) {
+				x -= 1;
+			}
+		}
+		
+		if(id == 1 || id == 2 || id == 3) {
+			world.setBlock(x, y, z, (id == 1 ? RCBlocks.transition_track : id == 2 ? RCBlocks.sloped_track : RCBlocks.transition_track_2));
+			TileEntityTrack track = (TileEntityTrack) world.getTileEntity(x, y, z);
+			track.direction = direction;
+			world.markBlockForUpdate(x, y, z);
+			if(direction == 0) {
+				z += 1;
+				y += 1;
+			}
+			else if(direction == 1) {
+				x -= 1;
+				y += id > 3 ? -1 : 1;
+			}
+			else if(direction == 2) {
+				z -= 1;
+				y += id > 3 ? -1 : 1;
+			}
+			else if(direction == 3) {
+				x -= 1;
+				y += id > 3 ? -1 : 1;
+			}
+		}
+		
+		if(id == 4 || id == 5 || id == 6) {
+			if(id == 6) {
+				y -= 1;
+			}
+			world.setBlock(x, y, z, (id == 4 ? RCBlocks.transition_track : id == 5 ? RCBlocks.sloped_track : RCBlocks.transition_track_2));
+			TileEntityTrack track = (TileEntityTrack) world.getTileEntity(x, y, z);
+			track.direction = direction;
+			world.markBlockForUpdate(x, y, z);
+			if(direction == 0) {
+				z += 1;
+				y += 1;
+			}
+			else if(direction == 1) {
+				x -= 1;
+				y += id > 3 ? -1 : 1;
+			}
+			else if(direction == 2) {
+				z += 1;
+				y += id > 3 ? -1 : 1;
+			}
+			else if(direction == 3) {
+				x -= 1;
+				y += id > 3 ? -1 : 1;
+			}
+			if(id == 4) {
+				y += 1;
+			}	
+		}
+		
+		if(id == 7) {
+			world.setBlock(x, y, z, RCBlocks.curved_track);
+			TileEntityTrack track = (TileEntityTrack) world.getTileEntity(x, y, z);
+			track.direction = direction;
+			world.markBlockForUpdate(x, y, z);
+			if(direction == 0) {
+				x -= 1;
+				direction = 3;
+			}
+			else if(direction == 1) {
+				direction = 2;
+				z -= 1;
+			}
+			else if(direction == 2) {
+				x += 1;
+				direction = 1;
+			}
+			else if(direction == 3) {
+				z += 1;
+				direction = 0;
+			}
+		}
+		
+		if(id == 8) {
+			world.setBlock(x, y, z, RCBlocks.loop);
+			TileEntityTrack track = (TileEntityTrack) world.getTileEntity(x, y, z);
+			track.direction = direction;
+			world.markBlockForUpdate(x, y, z);
+			
+			x -= 1;
+			z += 1;
+		}
+		
+		if(id == 9) {
+			if(direction == 3) {
+				direction = 0;
+			}
+			else {
+				direction += 1;
+			}
+		}
 	}
 
 	private void change(int id) {
